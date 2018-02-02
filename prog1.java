641316a5f1b91eac410abd9333a81e77b27854bb
Edgar Alvarez <edgarfabero85@gmail.com>
2018-02-02 11:05:06 +0100

    he escrito una nueva linea que indica mi localidad de nacimiento

--- prog1.java
+++ prog1.java
@@ -2,5 +2,6 @@ public class prog1 {
 	public static void main(String args[]){
 	System.out.println("Edgar Álvarez");
 	system.out.println("tengo 21 años");
+	system.out.println("vivo en Fabero");
  }
 }
